a00aed8f89e0374c00597f05bffa40f242fc3d26
root <root@Allens-PC.localdomain>
2020-04-04 22:49:59 -0400

    4/4/2020 10:49 PM

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
 
 const app = new express()
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+	res.render('index')
+})
+
+app.get('/about', (req, res)=>{
+	res.render('about')
+})
+
+app.get('/post', (req, res)=>{                                                                                               res.render('post')
+})
+
+app.get('/contact', (req, res)=>{                                                                                            res.render('contact')
+})
+
 app.listen(4000, ()=>{
-	console.log('App listening on port 4000')
+	console.log('App listening on port 4000'))
 })
