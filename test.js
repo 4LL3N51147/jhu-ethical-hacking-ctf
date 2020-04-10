95e78fd787e17d6df0b750f91dcbfab614cba3ba
Yilun Yang <yilunyang@Yiluns-MacBook-Pro.local>
2020-04-10 02:23:00 -0400

    4/10/2020 2:22 AM

---
+++ test.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost')
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser:true, useUnifiedTopology: true})
+
+BlogPost.create({title: '1',
+		 body: '2'}, (error, blogpost) => {
+			console.log(error, blogpost)
+})
